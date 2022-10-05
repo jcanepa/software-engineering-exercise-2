5440d9b64b5ef05c0b8aba8e9ce60bc2f1bb2ea4
Julian Canepa <julian.l.canepa@gmail.com>
2022-10-05 11:39:40 -0700

    Revert function rename to original function header, and preserve compatibility with external function calls.

--- main.rb
+++ main.rb
@@ -3,7 +3,7 @@
 @name = "Johanna Jackson"
 
 # "Mangle" the name by reversing or borgifying it.
-def mangle_name(name, borgify)
+def modified_name(name, borgify)
   return borgify_name(name) if borgify
   return reverse_name(name)
 end
@@ -11,11 +11,11 @@ end
 # Reverse the order of a given name.
 # Ex: "First Last" outputs "Last First"
 def reverse_name(name)
-  # Split the name into an array containing first and last
+  # Split the name into an array containing its parts
   split_name = name.split()
-  # Reverse array of name components so the array is [last, first]
+  # Reverse order of name components so the array is [last, first]
   reversed_name_components = split_name.reverse
-  # Return rejoined array of name components
+  # Return string of name components separated by spaces
   return reversed_name_components.join(' ')
 end
 
@@ -26,8 +26,9 @@ def borgify_name(name)
 end
 
 # demonstrate the original function still works
-puts "New name:      #{mangle_name(@name, false)}"
-puts "New borg name: #{mangle_name(@name, true)}"
+puts "New name:      #{modified_name(@name, false)}"
+puts "New borg name: #{modified_name(@name, true)}"
+
 # demonstrate cleaner code functions (refactored original)
 puts "New name:      #{reverse_name(@name)}"
 puts "New borg name: #{borgify_name(@name)}"
