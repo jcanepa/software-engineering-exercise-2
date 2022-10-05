bea9782c2cfc290d2d8d828c5a26d85d30acaa97
Julian Canepa <julian.l.canepa@gmail.com>
2022-10-05 11:30:07 -0700

    Clean up contents.

--- main.rb
+++ main.rb
@@ -4,25 +4,15 @@
 
 # Mangle the name by reversing it or borgifying it
 def modified_name(name, add_borg)
-  # Split the name into first and last in an array
-  split_name = name.split()
-  # Reverse the array of name components so the array is [last, first]
-  reversed_name_components = split_name.reverse
-
-  if add_borg
-    # Add borg to the end of the array
-    reversed_name_components << "Borg"
-  end
-
-  # Return rejoined array of name components
-  return reversed_name_components.join(' ')
+  return borgify_name(name) if add_borg
+  return reverse_name(name)
 end
 
 # return "First Last" as "Last First"
 def reverse_name(name)
-  # Split the name into first and last in an array
+  # Split the name into an array containing first and last
   split_name = name.split()
-  # Reverse the array of name components so the array is [last, first]
+  # Reverse array of name components so the array is [last, first]
   reversed_name_components = split_name.reverse
   # Return rejoined array of name components
   return reversed_name_components.join(' ')
@@ -33,8 +23,9 @@ def borgify_name(name)
   return reverse_name(name) + ' Borg'
 end
 
+# demonstrate the original function
 puts "New name:      #{modified_name(@name, false)}"
 puts "New borg name: #{modified_name(@name, true)}"
-
+# demonstrate clean code functions (refactored original)
 puts "New name:      #{reverse_name(@name)}"
 puts "New borg name: #{borgify_name(@name)}"
