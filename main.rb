fae8af8b16a8aaef7c9e04a419107e3ac2eb8c67
Julian Canepa <julian.l.canepa@gmail.com>
2022-10-05 11:35:05 -0700

    Rename main original function.

--- main.rb
+++ main.rb
@@ -2,13 +2,13 @@
 
 @name = "Johanna Jackson"
 
-# Mangle the name by reversing it or borgifying it
-def modified_name(name, add_borg)
+# "Mangle" the name by reversing or borgifying it
+def mangle_name(name, add_borg)
   return borgify_name(name) if add_borg
   return reverse_name(name)
 end
 
-# Reverse the order of a given name
+# Reverse the order of a given name.
 # Ex: "First Last" outputs "Last First"
 def reverse_name(name)
   # Split the name into an array containing first and last
@@ -19,15 +19,15 @@ def reverse_name(name)
   return reversed_name_components.join(' ')
 end
 
-# Reverses & appends "Borg" to a name,
+# Reverses & appends "Borg" to a name.
 # Ex: "Some Name" outputs "Name Some Borg"
 def borgify_name(name)
   return reverse_name(name) + ' Borg'
 end
 
 # demonstrate the original function still works
-puts "New name:      #{modified_name(@name, false)}"
-puts "New borg name: #{modified_name(@name, true)}"
-# demonstrate cleaned code functions (refactored original)
+puts "New name:      #{mangle_name(@name, false)}"
+puts "New borg name: #{mangle_name(@name, true)}"
+# demonstrate cleaner code functions (refactored original)
 puts "New name:      #{reverse_name(@name)}"
 puts "New borg name: #{borgify_name(@name)}"
